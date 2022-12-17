6c20e88eacc62e94d03cf9b29340f13eac462a4f
Tetiana Ruzhetska <tetiana.ruzhetska@gmail.com>
2022-12-17 11:14:38 +0200

    done

--- index.js
+++ index.js
@@ -17,3 +17,16 @@
 // const result = prompt("Яка офіційна назва JavaScript?");
 // const message = result === "ECMAScript" ? "Вірно" : "Не знаєте? ECMAScript!";
 // alert(message);
+
+
+//2. Напишіть програму, яка отримує від користувача через prompt()
+//число (кількість хвилин) і виведе в консоль
+//рядок у форматі годин і хвилин, тобто 70 має повернути у консоль "01:10"
+// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/String/padStart
+
+const question = prompt("Введіть число");
+const hours = String(Math.floor(Number(question) / 60)).padStart(2, "0");
+const minutes = String(Number(question) % 60).padStart(2, "0");
+const answer = `${hours}:${minutes}`;
+
+console.log(answer)
