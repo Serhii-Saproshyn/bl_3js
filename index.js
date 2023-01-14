0bba80459baec4729b73dcadb33d3749ccb01990
alisaromantsova <alis.romantsova@gmail.com>
2023-01-14 12:21:30 +0200

    task 3

--- index.js
+++ index.js
@@ -211,30 +211,40 @@
 // };
 // console.log(calcTotalPrice(stones, "emerald"));
 
-
 //4. Створіть об'єкт calculator з трьома методами
 //read(a, b) - приймає два аргумента та зберігає їх як властивості об'єкта під ключами prop1 та prop2
 //sum() перевіряє чи присутні  властивості prop1/prop2 та повертає їх суму, в противному випадку повертає null
 //mult() перевіряє чи присутні  властивості prop1/prop2 та повертає їх добуток, в противному випадку повертає null
 // Не забудьте, що математичні оператори працюють коректно тільки з числовими значеннями
 
-const calculator = {
-  read(a, b) {
-    if (Number(a) && Number(b)) {this.prop1 = a; this.prop2 = b
-    return
-    }
-    console.log('Is not a number')
-  },
-  sum() {
-    if (this.prop1 && this.prop2) {
-      return Number(this.prop1) + Number(this.prop2);
-    } return null;
-  },
-  mult() {if (this.prop1 && this.prop2) {
-      return this.prop1 * this.prop2;
-    } return null;},
-};
-calculator.read(2, '100')
-console.log("calculator", calculator)
+// const calculator = {
+//   read(a, b) {
+//     if (Number(a) && Number(b)) {this.prop1 = a; this.prop2 = b
+//     return
+//     }
+//     console.log('Is not a number')
+//   },
+//   sum() {
+//     if (this.prop1 && this.prop2) {
+//       return Number(this.prop1) + Number(this.prop2);
+//     } return null;
+//   },
+//   mult() {if (this.prop1 && this.prop2) {
+//       return this.prop1 * this.prop2;
+//     } return null;},
+// };
+// calculator.read(2, '100')
+// console.log("calculator", calculator)
 // console.log("calculator.sum", calculator.sum())
 // console.log("calculator.mult", calculator.mult())
+//6. Напишіть функцію updateObject, яка приймає об'єкт та перелік ключів
+//і повертає новий об'єкт без вказаних властивостей
+//Очікуваний результат ({ a: 1, b: 2, c: 3 }, 'b', 'c') => {a: 1}
+// const updateObject = (object, ...keys) => {
+//   const newObject = { ...object };
+//   for (const key of keys) {
+//     delete newObject[key];
+//   }
+//   return newObject;
+// };
+// console.log(updateObject({ a: 1, b: 2, c: 3 }, "b", "a", "c"));
