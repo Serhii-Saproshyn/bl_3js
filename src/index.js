fd8e40cb56cb76f13300e966f557d42f2ed9cafb
Serhii Saproshyn <112710917+Serhii-Saproshyn@users.noreply.github.com>
2023-03-11 12:05:49 +0200

    task-3

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import "./styles/normalize.css";
 import "./styles/index.css";
-import { getAllProducts, getProductById } from "./requests/products";
+import {
+  getAllProducts,
+  getProductById,
+  addNewProduct,
+} from "./requests/products";
 
 const singleProductFormRef = document.querySelector("#singleProductForm");
 const singleProductRef = document.querySelector("#singleProduct");
@@ -21,21 +25,51 @@ const singleProductRef = document.querySelector("#singleProduct");
 //   listProductsRef.innerHTML = markup;
 // });
 
-singleProductFormRef.addEventListener("submit", onSingleProductFormSubmit);
+// singleProductFormRef.addEventListener("submit", onSingleProductFormSubmit);
 
-async function onSingleProductFormSubmit(e) {
+// async function onSingleProductFormSubmit(e) {
+//   e.preventDefault();
+//   const idValue = e.target.elements.id.value;
+//   const {
+//     data: { title, description, price, images },
+//   } = await getProductById(idValue);
+
+//   const markup = `
+//       <h3>${title}</h3>
+//       <p>${description}</p>
+//       <p>Price:${price}$</p>
+//       <img width="150" height="100" src='${images[0]}'>
+//     `;
+
+//   singleProductRef.innerHTML = markup;
+// }
+
+const refs = {
+  form: document.querySelector("#newProductForm"),
+  newProduct: document.querySelector("#newProductSection"),
+};
+
+refs.form.addEventListener("submit", onNewProductFormSubmit);
+
+async function onNewProductFormSubmit(e) {
   e.preventDefault();
-  const idValue = e.target.elements.id.value;
+  const title = e.target.elements.title.value;
+  const price = e.target.elements.price.value;
+  const description = e.target.elements.description.value;
   const {
-    data: { title, description, price, images },
-  } = await getProductById(idValue);
-
+    data: {
+      title: newProductTitle,
+      description: newProductDescription,
+      price: newProductPrice,
+      id: newProductId,
+    },
+  } = await addNewProduct({ title, price, description });
   const markup = `
-      <h3>${title}</h3>
-      <p>${description}</p>
-      <p>Price:${price}$</p>
-      <img width="150" height="100" src='${images[0]}'>
-    `;
+       <h3>${newProductTitle}</h3>
+       <p>${newProductDescription}</p>
+       <p>Price:${newProductPrice}$</p>
+       <p>${newProductId}</p>
+     `;
 
-  singleProductRef.innerHTML = markup;
+  refs.newProduct.innerHTML = markup;
 }
