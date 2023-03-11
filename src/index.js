cf171a67ca18cac91d1d13c41f0420627576d43c
Ceres Rasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2023-03-11 17:41:35 +0200

    some change
    Добавив коментарі та описи Завдань

--- src/index.js
+++ src/index.js
@@ -7,75 +7,85 @@ import {
   deleteProductById,
 } from "./requests/products";
 
+// Задача 1.
+// Создай функцию для получения информации обо всех продуктах с последующим рендером списка.
+
+const listProductsRef = document.querySelector("#allProducts");
+getAllProducts().then(({ data: { products } }) => {
+  const markup = products
+    .map(({ title, description, price, images }) => {
+      return ` <li>
+    <h3>${title}</h3>
+    <p>${description}</p>
+    <p>Price:${price}$</p>
+    <img width="150" height="100" src='${images[0]}'>
+    </li>`;
+    })
+    .join("");
+  listProductsRef.innerHTML = markup;
+});
+
+// Задача 2.
+// Реализуй поиск и рендер информации о продукте по его ID.
+
 const singleProductFormRef = document.querySelector("#singleProductForm");
 const singleProductRef = document.querySelector("#singleProduct");
+singleProductFormRef.addEventListener("submit", onSingleProductFormSubmit);
 
-// const listProductsRef = document.querySelector("#allProducts");
-// getAllProducts().then(({ data: { products } }) => {
-//   const markup = products
-//     .map(({ title, description, price, images }) => {
-//       return ` <li>
-//       <h3>${title}</h3>
-//       <p>${description}</p>
-//       <p>Price:${price}$</p>
-//       <img width="150" height="100" src='${images[0]}'>
-//     </li>`;
-//     })
-//     .join("");
-//   console.log(markup);
-//   listProductsRef.innerHTML = markup;
-// });
+async function onSingleProductFormSubmit(e) {
+  e.preventDefault();
+  const idValue = e.target.elements.id.value;
+  const {
+    data: { title, description, price, images },
+  } = await getProductById(idValue);
 
-// singleProductFormRef.addEventListener("submit", onSingleProductFormSubmit);
+  const markup = `
+      <h3>${title}</h3>
+      <p>${description}</p>
+      <p>Price:${price}$</p>
+      <img width="150" height="100" src='${images[0]}'>
+    `;
 
-// async function onSingleProductFormSubmit(e) {
-//   e.preventDefault();
-//   const idValue = e.target.elements.id.value;
-//   const {
-//     data: { title, description, price, images },
-//   } = await getProductById(idValue);
+  singleProductRef.innerHTML = markup;
+}
 
-//   const markup = `
-//       <h3>${title}</h3>
-//       <p>${description}</p>
-//       <p>Price:${price}$</p>
-//       <img width="150" height="100" src='${images[0]}'>
-//     `;
+// Задача 3.
+// Создай и отрендери новый продукт.
 
-//   singleProductRef.innerHTML = markup;
-// }
+const refs = {
+  form: document.querySelector("#newProductForm"),
+  newProduct: document.querySelector("#newProductSection"),
+};
 
-// const refs = {
-//   form: document.querySelector("#newProductForm"),
-//   newProduct: document.querySelector("#newProductSection"),
-// };
+refs.form.addEventListener("submit", onNewProductFormSubmit);
 
-// refs.form.addEventListener("submit", onNewProductFormSubmit);
+async function onNewProductFormSubmit(e) {
+  e.preventDefault();
+  const title = e.target.elements.title.value;
+  const price = e.target.elements.price.value;
+  const description = e.target.elements.description.value;
+  const {
+    data: {
+      title: newProductTitle,
+      description: newProductDescription,
+      price: newProductPrice,
+      id: newProductId,
+    },
+  } = await addNewProduct({ title, price, description });
+  const markup = `
+       <h3>${newProductTitle}</h3>
+       <p>${newProductDescription}</p>
+       <p>Price:${newProductPrice}$</p>
+       <p>${newProductId}</p>
+     `;
 
-// async function onNewProductFormSubmit(e) {
-//   e.preventDefault();
-//   const title = e.target.elements.title.value;
-//   const price = e.target.elements.price.value;
-//   const description = e.target.elements.description.value;
-//   const {
-//     data: {
-//       title: newProductTitle,
-//       description: newProductDescription,
-//       price: newProductPrice,
-//       id: newProductId,
-//     },
-//   } = await addNewProduct({ title, price, description });
-//   const markup = `
-//        <h3>${newProductTitle}</h3>
-//        <p>${newProductDescription}</p>
-//        <p>Price:${newProductPrice}$</p>
-//        <p>${newProductId}</p>
-//      `;
+  refs.newProduct.innerHTML = markup;
+}
 
-//   refs.newProduct.innerHTML = markup;
-// }
-
-// Task-4
+// Задача 4.
+// Удалите продукт по его ID.
+// Если успешно, выведите через alert SUCCESS и инфо
+// об удаленном продукте, в противном случае выведите ERROR и причину ошибки.
 
 const deletionProductForRefs = document.querySelector("#deletionProductForm");
 
