719ef7e229c876d5064afe32094f297b41e8f2ca
Ceres Rasmussen <112823848+CeresRasmussen@users.noreply.github.com>
2023-03-11 11:37:43 +0200

    task-2

--- src/index.js
+++ src/index.js
@@ -1,18 +1,41 @@
 import "./styles/normalize.css";
 import "./styles/index.css";
-import { getAllProducts } from "./requests/products";
-const listProductsRef = document.querySelector("#allProducts");
-getAllProducts().then(({ data: { products } }) => {
-  const markup = products
-    .map(({ title, description, price, images }) => {
-      return ` <li>
+import { getAllProducts, getProductById } from "./requests/products";
+
+const singleProductFormRef = document.querySelector("#singleProductForm");
+const singleProductRef = document.querySelector("#singleProduct");
+
+// const listProductsRef = document.querySelector("#allProducts");
+// getAllProducts().then(({ data: { products } }) => {
+//   const markup = products
+//     .map(({ title, description, price, images }) => {
+//       return ` <li>
+//       <h3>${title}</h3>
+//       <p>${description}</p>
+//       <p>Price:${price}$</p>
+//       <img width="150" height="100" src='${images[0]}'>
+//     </li>`;
+//     })
+//     .join("");
+//   console.log(markup);
+//   listProductsRef.innerHTML = markup;
+// });
+
+singleProductFormRef.addEventListener("submit", onSingleProductFormSubmit);
+
+async function onSingleProductFormSubmit(e) {
+  e.preventDefault();
+  const idValue = e.target.elements.id.value;
+  const {
+    data: { title, description, price, images },
+  } = await getProductById(idValue);
+
+  const markup = `
       <h3>${title}</h3>
       <p>${description}</p>
       <p>Price:${price}$</p>
       <img width="150" height="100" src='${images[0]}'>
-    </li>`;
-    })
-    .join("");
-  console.log(markup);
-  listProductsRef.innerHTML = markup;
-});
+    `;
+
+  singleProductRef.innerHTML = markup;
+}
